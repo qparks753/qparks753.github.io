05d80c44b89156ac50d8ba788b0ab1f55bd5afb7
IB_Student <student@InnovatBhamsMBP.attlocal.net>
2022-03-10 14:45:57 -0600

    added base files

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+let loader = document.querySelector(".loaderelement");
+window.addEventListener("load",function(){
+    loader.style.display = "none";
+});
+
+let link = document.querySelector("#social-linkedin");
+link.addEventListener("click", function(){    
+    location.href = 'https://www.linkedin.com/in/quinton-parks/';
+});
+
+let link1 = document.querySelector("#social-github");
+link1.addEventListener("click", function(){    
+    location.href = 'https://github.com/qparks753';
+});
+
+let link2 = document.querySelector('.resumebtn');
+link2.addEventListener("click",()=>{
+    location.href = "/images/Quinton Parks Resume 2022 R.pdf";
+});
+
